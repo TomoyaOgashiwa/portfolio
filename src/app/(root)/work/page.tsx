d27e60dbeef6cf7ebe3b79d35ba52465abb30c5a
TomoyaOgashiwa <tomoya07oga@gmail.com>
2025-03-04 23:21:21 -0800

    Revert "test"
    This reverts commit 9058187abad1d26253d351eff3643742311e8bb4.

--- src/app/(root)/work/page.tsx
+++ src/app/(root)/work/page.tsx
@@ -1,29 +1,23 @@
-"use client";
-
 import Image from "next/image";
 import { CareerHistory } from "../constants/contants";
-// import type { Metadata } from "next";
-// import Link from "next/link";
-import { useRouter } from "next/navigation";
+import type { Metadata } from "next";
+import Link from "next/link";
 
-// export const metadata: Metadata = {
-//   title: "Portfolio - Work",
-//   description: "Tomoya's Career Journey",
-// };
+export const metadata: Metadata = {
+  title: "Portfolio - Work",
+  description: "Tomoya's Career Journey",
+};
 
 export default function Page() {
-  const router = useRouter();
   return (
     <section className="flex flex-col gap-4 max-w-3xl px-4 mx-auto sm:px-0 dark:text-white">
       <h1 className="mr-auto text-2xl font-bold">Work</h1>
       <ul className="list-none flex flex-col gap-6">
         {CareerHistory.map((c) => (
           <li className="border-b border-gray-200 pt-2 pb-4" key={c.name}>
-            <button
-              type="button"
+            <Link
               className="sm:grid sm:grid-cols-2 gap-6 items-center"
-              // href={`/work/${c.name}`}
-              onClick={() => router.push(`/work/${c.name}`)}
+              href={`/work/${c.name}`}
             >
               <Image
                 layout="responsive"
@@ -38,7 +32,7 @@ export default function Page() {
                 <p className="text-base">{c.abstraction}</p>
                 <p className="hidden sm:block text-sm">{c.description}</p>
               </div>
-            </button>
+            </Link>
           </li>
         ))}
       </ul>
